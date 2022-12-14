5084a6a8b182aca1bca5870ef0483321b9ca5785
Oleksandr Druzhynin <aleks_str78@ukr.net>
2022-12-14 20:02:16 +0200

    друга з промісами

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,13 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 
 const player = new Player(iframe);
 
 const lastCurrentTime = localStorage.getItem('videoplayer-current-time');
 
 if (lastCurrentTime) {
-    // console.log('​lastCurrentTime', lastCurrentTime);
     player
       .setCurrentTime(lastCurrentTime)
       .then(function (seconds) {
@@ -33,13 +32,3 @@ player.on(
     localStorage.setItem('videoplayer-current-time', seconds);
   }, 1000)
 );
-
-
-
-
-
-
-
-
-    // const iframe = document.querySelector('iframe');
-    // const player = new Vimeo.Player(iframe);
