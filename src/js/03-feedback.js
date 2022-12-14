7aca2f51eb230749b77f0f25c5bd95a3834d8d39
Oleksandr Druzhynin <aleks_str78@ukr.net>
2022-12-14 22:39:13 +0200

    з готова

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = {};
+
+fillingPreviousValues();
+
+function fillingPreviousValues() {
+  let savedMessage = localStorage.getItem(STORAGE_KEY);
+  savedMessage = JSON.parse(savedMessage);
+
+
+    if (savedMessage) {
+
+      if (savedMessage.email) {
+          email.value = savedMessage.email;
+          formData.email = savedMessage.email;
+      }
+    if (savedMessage.message) {
+        message.value = savedMessage.message;
+        formData.message = savedMessage.message;
+    }
+  }
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
